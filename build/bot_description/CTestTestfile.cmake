3b38ff0eb5e52e15ce4f990e6dc99212b5a6c483
“nsk-05” <sanjaykumar.nsk5@gmail.com>
2022-10-15 02:13:05 +0530

    The bot simulation has been done . The functionalit of the warehouse sorting robot need to be added

---
+++ build/bot_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/nsk/Desktop/project_1/src/bot_description
+# Build directory: /home/nsk/Desktop/project_1/build/bot_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/nsk/Desktop/project_1/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/nsk/Desktop/turtle/devel;/opt/ros/noetic'.split(';') if '/home/nsk/Desktop/turtle/devel;/opt/ros/noetic' != '' else []
