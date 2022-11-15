558380a22717504fe7648e597b73f7a71b28f872
“nsk-05” <sanjaykumar.nsk5@gmail.com>
2022-11-15 22:58:57 +0530

    removing build and devel folder sfrom the git repo

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/nsk/Desktop/project_1/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/nsk/Desktop/project_1/devel;/home/nsk/Desktop/turtle/devel;/opt/ros/noetic'.split(';') if '/home/nsk/Desktop/project_1/devel;/home/nsk/Desktop/turtle/devel;/opt/ros/noetic' != '' else []
--- build/grid_description /CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nsk/Desktop/project_1/src/grid_description 
-# Build directory: /home/nsk/Desktop/project_1/build/grid_description 
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
