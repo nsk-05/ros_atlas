7db41258f47f2954e16bae8f50abc9d5cfafcd8b
“nsk-05” <sanjaykumar.nsk5@gmail.com>
2022-12-27 18:22:33 +0000

    map_saver pkg intial commit. Have to alter this pkg to create clean map.

---
+++ src/bot_vision/src/map_create.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+import rospy
+# ROS Image message
+from std_srvs.srv import Trigger,TriggerResponse
+from sensor_msgs.msg import Image
+# ROS Image message -> OpenCV2 image converter
+from cv_bridge import CvBridge, CvBridgeError
+# OpenCV2 for saving an image
+import cv2
+import os
+
+
+class Map_save():
+    def __init__(self) -> None:
+        self.save=0
+        # Instantiate CvBridge
+        self.bridge = CvBridge()
+
+        # Define your image topic
+        self.image_topic = "/camera/image_raw"
+        
+
+    def image_callback(self,msg):
+        print("Received an image!")
+        try:
+            # Convert your ROS Image message to OpenCV2
+            self.cv2_img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+
+        except CvBridgeError as e:
+            print(e)
+        
+    def map_service(self,req):
+        os.chdir("maps")
+        gray=cv2.cvtColor(self.cv2_img,cv2.COLOR_BGR2GRAY)
+        cv2.imwrite('camera_image.pgm', gray)
+        res=TriggerResponse()
+        res.message="image saved"
+        res.success=True
+        return res
+
+
+if __name__ == '__main__':
+    # main()
+    MapSaver=Map_save()
+    rospy.init_node('map_saver')
+    rospy.Subscriber(MapSaver.image_topic, Image, MapSaver.image_callback)
+    rospy.Service("/map_save",Trigger,MapSaver.map_service)
+    rospy.spin()
