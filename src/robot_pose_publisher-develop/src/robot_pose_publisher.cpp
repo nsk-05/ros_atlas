119b5f717a2c30aa27805a0f5cc07d57493f904d
“nsk-05” <sanjaykumar.nsk5@gmail.com>
2023-01-04 00:43:51 +0530

    map to base link .
    just a daily update.

--- src/robot_pose_publisher-develop/src/robot_pose_publisher.cpp
+++ src/robot_pose_publisher-develop/src/robot_pose_publisher.cpp
@@ -35,7 +35,7 @@ int main(int argc, char ** argv)
   ros::Publisher p_pub;
 
   nh_priv.param<std::string>("map_frame",map_frame,"/map");
-  nh_priv.param<std::string>("base_frame",base_frame,"/base_footprint");
+  nh_priv.param<std::string>("base_frame",base_frame,"/odom");
   nh_priv.param<double>("publish_frequency",publish_frequency,10);
   nh_priv.param<bool>("is_stamped", is_stamped, false);
 
