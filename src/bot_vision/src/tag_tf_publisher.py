70f1ab9c97f4a14474e5e94d798b568ed11f43a0
“nsk-05” <sanjaykumar.nsk5@gmail.com>
2023-01-14 23:29:18 +0530

    planner configs

--- src/bot_vision/src/tag_tf_publisher.py
+++ src/bot_vision/src/tag_tf_publisher.py
@@ -21,19 +21,20 @@ class tag_to_odom():
             current_time=rospy.Time.now()
             pose=i.pose.pose.pose
             x=pose.position.x
-            y=-1 * pose.position.y 
+            y=-1 * pose.position.y
+            z=(-1 * pose.position.z)+5
 
             x_=pose.orientation.x
             y_=pose.orientation.y
             z_=pose.orientation.z
             w_=pose.orientation.w
             # print(x,y,x_,y_,z_,w_)
-            euler=tf.transformations.euler_from_quaternion((x_,y_,z_,w_))
+            # euler=tf.transformations.euler_from_quaternion((x_,y_,z_,w_))
             # yaw=euler[2]-2.106
             # quat=tf.transformations.quaternion_from_euler(0,0,yaw)
-            print(euler)
+            # print(euler)
             #broadcast transform
-            # self.odom_broadcaster.sendTransform((x, y, 0.),quat,current_time,child_frame, "odom")
+            self.odom_broadcaster.sendTransform((x, y, z),(x_,y_,z_,w_),current_time,child_frame, "camera_link_optical")
 
 if __name__ == '__main__':
     tag2odom=tag_to_odom()
